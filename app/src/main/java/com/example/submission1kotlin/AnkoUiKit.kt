f45e60e7b90240a6f82ae004c3ee3efc18d73325
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-03 06:54:35 +0700

    Creating AnkoUIKit class

---
+++ app/src/main/java/com/example/submission1kotlin/AnkoUiKit.kt
@@ -0,0 +1,11 @@
+package com.example.submission1kotlin
+
+import android.view.View
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+
+class AnkoUiKit : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>): View {
+
+    }
+}
