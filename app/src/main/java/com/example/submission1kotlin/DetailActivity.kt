d1ab2888756ce7b02e5f6fc0e9f8f30a59b9fe83
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-03 20:31:18 +0700

    Updating color, resource, and changing DetailActivity Styles

--- app/src/main/java/com/example/submission1kotlin/DetailActivity.kt
+++ app/src/main/java/com/example/submission1kotlin/DetailActivity.kt
@@ -1,9 +1,10 @@
 package com.example.submission1kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.item_detail.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
@@ -20,7 +21,12 @@ class DetailActivity : AppCompatActivity(), AnkoLogger {
         val description = resources.getStringArray(R.array.club_desc)
 
         Glide.with(this).load(image.getResourceId(detailKey,0)).into(club_img_logo)
+        Glide.with(this).load(image.getResourceId(detailKey,0)).into(club_poster)
         club_text_name.text = name[detailKey]
         club_text_desc.text = description[detailKey]
+
+        collapsing_bar.title = name[detailKey]
+        collapsing_bar.setCollapsedTitleTextColor(resources.getColor(R.color.colorWhite))
+        collapsing_bar.setExpandedTitleColor(resources.getColor(R.color.colorWhite))
     }
 }
