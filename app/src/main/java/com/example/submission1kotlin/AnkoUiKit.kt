e6946a7579f93be63dd9e9bbf26a98f956473bc9
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-03 07:17:24 +0700

    Updating Anko Layout

--- app/src/main/java/com/example/submission1kotlin/AnkoUiKit.kt
+++ app/src/main/java/com/example/submission1kotlin/AnkoUiKit.kt
@@ -1,11 +1,53 @@
 package com.example.submission1kotlin
 
+import android.content.Context
 import android.view.View
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 class AnkoUiKit : AnkoComponent<MainActivity> {
-    override fun createView(ui: AnkoContext<MainActivity>): View {
+    var recycler: RecyclerView? = null
 
+    override fun createView(ui: AnkoContext<MainActivity>)= with(ui) {
+        linearLayout{
+            orientation = LinearLayout.VERTICAL
+            recycler =recyclerView {
+                lparams(width = matchParent, height = matchParent)
+            }
+        }
     }
+}
+
+class ListAnko : AnkoComponent<Context>{
+    var image: ImageView? = null
+    var name: TextView? = null
+    override fun createView(ui: AnkoContext<Context>): View = with(ui) {
+        linearLayout{
+            orientation = LinearLayout.VERTICAL
+            padding = dip(16)
+
+            imageView{
+                id = Ids.image
+            }.lparams(width = dip(72), height = dip(72)){
+                margin = dip(16)
+            }
+
+            textView {
+                id = Ids.name
+                textSize = sp(20).toFloat()
+            }.lparams(width = wrapContent, height = wrapContent){
+                topMargin = dip(30)
+            }
+        }
+    }
+
+    object Ids {
+        const val image = 1
+        const val name = 2
+    }
+
 }
