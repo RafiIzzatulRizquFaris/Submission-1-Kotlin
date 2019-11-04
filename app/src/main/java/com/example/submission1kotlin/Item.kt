aa686f5e556bd50deb72c00d3f6b0bd815c041da
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-04 15:15:54 +0700

    Update Item.kt for parcelable

--- app/src/main/java/com/example/submission1kotlin/Item.kt
+++ app/src/main/java/com/example/submission1kotlin/Item.kt
@@ -1,9 +1,37 @@
 package com.example.submission1kotlin
 
-import android.media.Image
+import android.os.Parcel
+import android.os.Parcelable
 
-class Item (
+data class Item (
     val name: String?,
     val desc: String?,
     val image: Int?
+
+    ) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int
     )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(desc)
+        parcel.writeValue(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Item> {
+        override fun createFromParcel(parcel: Parcel): Item {
+            return Item(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Item?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
